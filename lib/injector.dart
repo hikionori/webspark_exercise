9804f5e62d3046cd5a545a3544776d36ca5e66b9
Danila Verbinskiy <work.danv4656@gmail.com>
2024-02-28 12:19:40 +0200

    Add injector and main.dart files

---
+++ lib/injector.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:test_exercise/core/api/app_api.dart';
+import 'package:test_exercise/data/repositories/fetch_data_repository.dart';
+import 'package:test_exercise/domain/repositories/fetch_data_repository.dart';
+import 'package:test_exercise/domain/usecases/fetch_data_usecase.dart';
+import 'package:test_exercise/domain/usecases/send_data_usecase.dart';
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerLazySingleton(() => FetchDataUseCase(getIt()));
+  getIt.registerLazySingleton(() => SendDataUseCase(getIt()));
+
+  getIt.registerLazySingleton<FetchDataRepository>(() => FetchDataRepositoryImpl(getIt()));
+
+  getIt.registerLazySingleton<Dio>(() => Dio());
+  getIt.registerLazySingleton<AppApi>(() => AppApi(getIt<Dio>()));
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:test_exercise/injector.dart';
+import 'package:test_exercise/presentation/pages/home/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setup();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
